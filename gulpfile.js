2939a291977ece7d733d3633b9d218b9b87ef3d5
Michael Puckett <michaelcpuckett@gmail.com>
2016-11-13 13:28:11 -0500

    Replace old site with new site

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,38 @@
 var gulp = require('gulp');
-var livereload = require('gulp-livereload');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
 var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer-core');
-var minifycss = require('csswring');
+var autoprefixer = require('autoprefixer');
+var cssVariables = require('postcss-custom-properties');
+var customMedia = require('postcss-custom-media');
 var mustache = require('gulp-mustache-plus');
-var templateData = require('./src/mustache/_templateData.json');
 var del = require('del');
+
+var templateData = require('./src/index.json');
 
 gulp.task('clean', function (cb) {
     return del('./public/', cb);
 });
 
 gulp.task('styles', function () {
-    return gulp.src('./src/scss/*.scss')
-        .pipe(sourcemaps.init({
-            inline: true
-        }))
-        .pipe(sass())
+    return gulp.src('./src/*.css')
         .pipe(postcss([
-            autoprefixer({
-                browsers: ['> 2%']
-            })
+            cssVariables(),
+            customMedia(),
+            // autoprefixer({
+            //     browsers: ['> 1%']
+            // })
         ]))
-        .pipe(gulp.dest('./public/css/'))
-				.pipe(livereload());
+        .pipe(gulp.dest('./public/'))
 });
 
 gulp.task('markup', function () {
-    return gulp.src('./src/mustache/*.mustache')
-        .pipe(mustache(templateData, {}, templateData['_partials']))
-        .pipe(gulp.dest('./public/'))
-				.pipe(livereload());
+    return gulp.src('./src/*.mustache')
+        .pipe(mustache(templateData))
+        .pipe(gulp.dest('./public/'));
 });
 
 gulp.task('assets', function () {
-    return gulp.src('./src/assets/**')
-        .pipe(gulp.dest('./public/assets/'));
+    return gulp.src(['./src/*.png', './src/*.jpg', './src/*.html', './src/*.pdf'])
+        .pipe(gulp.dest('./public/'));
 });
 
 gulp.task('default', ['styles', 'markup', 'assets']);
-
-gulp.task('watch', function () {
-    gulp.watch('./src/scss/**', ['styles']);
-    gulp.watch('./src/mustache/**', ['markup']);
-    gulp.watch('./src/assets/**', ['assets']);
-});
