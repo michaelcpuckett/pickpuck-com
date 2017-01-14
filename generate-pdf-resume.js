fb82e518ee710c3265ef46318890e792e3e55f02
Michael Puckett <michaelcpuckett@gmail.com>
2017-01-13 23:25:48 -0500

    generate pdf

--- generate-pdf-resume.js
+++ generate-pdf-resume.js
@@ -1,16 +1,21 @@
 var fs = require('fs');
 var pdf = require('html-pdf');
-var html = fs.readFileSync('./index.html', 'utf8');
-var options = { format: 'Letter',
-"base": "file:///Users/mpuckett/sites/pickpuck-com/public/",
-"border": {
-    "top": ".85in",            // default is 0, units: mm, cm, in, px
-    "right": ".85in",
-    "bottom": ".85in",
-    "left": ".85in"
-  } };
 
-pdf.create(html, options).toFile('./resume.pdf', function(err, res) {
-  if (err) return console.log(err);
-  console.log(res); // { filename: '/app/businesscard.pdf' }
-});
+
+module.exports = function () {
+    var html = fs.readFileSync('./phantom/index.html', 'utf8');
+    var options = {
+        format: 'Letter',
+        "base": "file:///Users/mpuckett/sites/pickpuck-com/phantom/",
+        "border": {
+            "top": ".85in",            // default is 0, units: mm, cm, in, px
+            "right": ".85in",
+            "bottom": ".85in",
+            "left": ".85in"
+        }
+    };
+    pdf.create(html, options).toFile('./public/resume.pdf', function(err, res) {
+          if (err) return console.log(err);
+          console.log(res);
+        });
+}
--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,18 @@ var cssVariables = require('postcss-custom-properties');
 var customMedia = require('postcss-custom-media');
 var mustache = require('gulp-mustache-plus');
 var del = require('del');
+var rename = require('gulp-rename');
 
 var templateData = require('./src/index.json');
 
+var generatePdf = require('./generate-pdf-resume');
+
 gulp.task('clean', function (cb) {
-    return del('./public/', cb);
+    return del(['./public/','./phantom/.'], cb);
 });
 
 gulp.task('styles', function () {
-    return gulp.src('./src/*.css')
+    return gulp.src(['./src/style.css','./src/print.css'])
         .pipe(postcss([
             cssVariables(),
             customMedia(),
@@ -22,17 +25,41 @@ gulp.task('styles', function () {
             // })
         ]))
         .pipe(gulp.dest('./public/'))
+        .pipe(gulp.dest('./phantom/'))
+});
+
+gulp.task('phantom:styles', ['public'], function () {
+    return gulp.src(['./src/phantom.css'])
+        .pipe(rename('print.css'))
+        .pipe(postcss([
+            cssVariables(),
+            customMedia(),
+            // autoprefixer({
+            //     browsers: ['> 1%']
+            // })
+        ]))
+        .pipe(gulp.dest('./phantom/'))
 });
 
 gulp.task('markup', function () {
     return gulp.src('./src/*.mustache')
         .pipe(mustache(templateData))
-        .pipe(gulp.dest('./public/'));
+        .pipe(gulp.dest('./public/'))
+        .pipe(gulp.dest('./phantom/'));
 });
 
 gulp.task('assets', function () {
     return gulp.src(['./src/*.png', './src/*.jpg', './src/*.html', './src/*.pdf'])
-        .pipe(gulp.dest('./public/'));
+        .pipe(gulp.dest('./public/'))
+        .pipe(gulp.dest('./phantom/'));
 });
 
-gulp.task('default', ['styles', 'markup', 'assets']);
+gulp.task('public', ['styles', 'markup', 'assets']);
+
+gulp.task('phantom:generate', ['phantom:styles'], function () {
+    generatePdf()
+});
+
+gulp.task('phantom', ['phantom:generate'])
+
+gulp.task('default', ['phantom'])
